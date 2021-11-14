22afab921bd247503122521a11ebba783459f828
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2021-11-14 18:13:46 +0530

    all crud operations without auth

---
+++ client/src/actions/posts.js
@@ -0,0 +1,55 @@
+import {FETCH_ALL,CREATE,UPDATE,DELETE,LIKE} from '../constants/actionTypes';
+import * as api from '../api';
+
+// Action Creators
+
+// firstly we make api reuests i.e make changes in the database and then return it
+// then we dispatch the respective action to change the state (by calling the reducers) => this is for frontend
+export const getPosts = () => async (dispatch) => {
+    try {
+        const {data} = await api.fetchPosts();
+
+        // dispatch the action
+        dispatch({type:FETCH_ALL,payload:data}); //it will go to posts reducer (FETCH_ALL is action)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+
+        dispatch({type:CREATE,payload:data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+       const {data} = await api.updatePost(id,post);
+
+       dispatch({type:UPDATE,payload:data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+        dispatch({type:DELETE, payload:id});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const {data} = await api.likePost(id);
+        dispatch({type:LIKE, payload:data});
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express()
+dotenv.config();
+
+app.use(express.json({limit:"30mb", extended:true}));
+app.use(express.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+// routes
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=>console.log(err.message));
+
+
